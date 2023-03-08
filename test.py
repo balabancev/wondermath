75f156ddba46d87c060279241008da8406881e0e
Alex <Alex.Balabantsev@lseg.com>
2023-03-08 17:40:21 +0000

    the math

--- test.py
+++ test.py
@@ -1,8 +1,26 @@
 import json
 import requests
 
-def main(question):
+def main(question):          
     print(question)
+    factor = question["factor"]
+    starting_multiplier = question["starting_multiplier"]
+    multiplier_inc = question["multiplier_inc"]
+    starting_base = question["starting_base"]
+    base_inc = question["base_inc"]
+    print("Let me see:")
+
+    for i in range(0, 10):
+        multiplier = starting_multiplier + multiplier_inc * i
+        base = starting_base +  base_inc * i
+        res = factor * multiplier
+        s = ""        
+        while res:
+            s = str(res % base) + s
+            res //= base
+        assert int(s) == 12 + i, "Oh dear! I shall never get to twenty at that rate!"
+
+        print(f"{factor} times {multiplier} is {s}")
 
 def test():
     with open("response.json", "r") as f:
