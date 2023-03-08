149edee69bbb65e528389039549fd34b32c22ee5
Alex <Alex.Balabantsev@lseg.com>
2023-03-08 17:38:44 +0000

    better start

--- test.py
+++ test.py
@@ -1,25 +1,11 @@
 import json
 import requests
 
+def main(question):
+    print(question)
 
-f = requests.get("https://blex18.free.beeceptor.com/wonderland", proxies={"https": "http://localhost:3128"}, verify=False)
-question = f.json()
-print(question)
-factor = question["factor"]
-starting_multiplier = question["starting_multiplier"]
-multiplier_inc = question["multiplier_inc"]
-starting_base = question["starting_base"]
-base_inc = question["base_inc"]
-print("Let me see:")
+def test():
+    with open("response.json", "r") as f:
+        main(json.loads(f.read()))
 
-for i in range(0, 10):
-    multiplier = starting_multiplier + multiplier_inc * i
-    base = starting_base +  base_inc * i
-    res = factor * multiplier
-    s = ""        
-    while res:
-        s = str(res % base) + s
-        res //= base
-    assert int(s) == 12 + i, "Oh dear! I shall never get to twenty at that rate!"
-
-    print(f"{factor} times {multiplier} is {s}")
+test()
